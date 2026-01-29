01120b436fadfdbff79f0cf580b697c588dbb6b4
agendasisreg <agendasisreg.cra@gmail.com>
2026-01-29 16:01:20 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,10 @@
 // config/config.js
 const CONFIG = {
   SISTEMA: "Agenda SISREG Amazonas",
-  SUBTITULO: "Sistema de Gestão de Agendas",
-  VERSAO: "2.0.0 (Definitiva)",
-  DESENVOLVEDOR: "VerasEurípedes",
+  VERSAO: "2.0.0",
+  DESENVOLVEDOR: "VerasEurípedes/Empresa",
   ANO: "2026",
-  
-  // URL do Google Apps Script (Você colará aqui após publicar a planilha)
-  URL_SHEETS: "https://script.google.com/macros/s/AKfycbx-aj2CuhiUf9eCiLJjfMCchIK8zyIiupNj6_qWhYJyThFqahRfP5ruT0JBzjpgiAAL/exec", 
-  
-  // Configurações de expiração de login (em horas)
+  // A URL da API fica APENAS aqui.
+  URL_API: "https://script.google.com/macros/s/AKfycbx-aj2CuhiUf9eCiLJjfMCchIK8zyIiupNj6_qWhYJyThFqahRfP5ruT0JBzjpgiAAL/exec",
   SESSION_EXPIRE: 24
 };
