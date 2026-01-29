ceba54fd651e233e453ffc6ace3d85f5d9e46c9c
agendasisreg <agendasisreg.cra@gmail.com>
2026-01-29 11:42:56 -0300

    Delete shared/components/index.js

--- shared/components/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Arquivo de exportação consolidada dos componentes
- * Facilita a importação de todos os componentes de uma vez
- */
-
-export { renderHeader } from './header.js';
-export { renderFooter } from './footer.js';
-export { renderNavbar } from './navbar.js';
